075a0c2ac6289c915ffad954d3bc341c26e9cd7c
Hmrinal <mrinaltyagi1912@gmail.com>
2020-10-05 18:27:50 +0530

    First Commit

---
+++ temp.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+#importing libraries
+from bs4 import BeautifulSoup as Bs
+import requests
+
+#method to get the price of gold
+def get_Price(url):
+
+    #Getting the request from url
+    data = requests.get(url)
+
+    #converting the text
+    soup = BS(data.text, 'html.parser')
+
+    #finding metha info for current price
+    ans = soup.find("div", class_ ="BNeawe s3v9rd AP7Wnd").text
+
+    #returning the price
+    return ans
+
+# url of the gold price
+url = "https://www.google.com/search?q=gold+price"
+
+#calling the get_price method
+ans = get_price(url)
+
+#printing the ans
+print(ans)
